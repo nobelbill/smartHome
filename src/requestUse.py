9ce7a2e68a291befb5a2c74508298992354c8655
nobelbill <seongyeol.yun@sk.com>
2015-06-24 16:17:37 +0900

    modify rq

--- src/requestUse.py
+++ src/requestUse.py
@@ -1,25 +1,44 @@
 __author__ = '1002097'
-
+#-*- coding: utf-8 -*-
 import requests
 import Weather
 import ConfigParser
+import sys
+import MakeTalkString
 
 class RequestUse :
+
+    config = ConfigParser.RawConfigParser()
+    def __init__(self):
+
+        self.config.read('config.properties')
 
     def headerMake(self,value) :
         return {'appKey' : value}
 
     def getWeather(self) :
-        config = ConfigParser.RawConfigParser()
-        config.read('config.properties')
-        url = config.get('BASIC','weather.url')
-        ra= requests.get(url,headers= self.headerMake(config.get('BASIC','weather.key')))
+
+        url = self.config.get('BASIC','weather.url')
+        ra= requests.get(url,headers= self.headerMake(self.config.get('BASIC','weather.key')))
         we = Weather.Weather()
 
         we.setRawJson(ra.text)
         we.parseWeather()
 
-        print we.getcloud()
+    def getMp3File(self,value):
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        ttsUrl= self.config.get('TTS','base.url') + value
+        response = requests.get(ttsUrl,headers=headers)
+        f = open('workfile.mp3', 'w+b')
+        f.write(response.content)
+        f.close()
 
+
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 ru = RequestUse()
-ru.getWeather()
+mts=MakeTalkString.MakeTalkString()
+
+ru.getMp3File( str((mts.makeTodayWeather(1))))
